2a745e48c69ed975aeed0b08b9cf6a6cb6868f39
carlov20 <carlov20@users.noreply.github.com>
2022-05-19 11:14:05 +0100

    WFP-1057 added security config to call community api

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,8 +7,16 @@ configurations {
   testImplementation { exclude(group = "org.junit.vintage") }
 }
 
+val springDocVersion by extra("1.6.8")
+
 dependencies {
   implementation("org.springframework.boot:spring-boot-starter-webflux")
+  implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
+  implementation("org.springframework.boot:spring-boot-starter-oauth2-client")
+
+  implementation("org.springdoc:springdoc-openapi-webflux-ui:$springDocVersion")
+  implementation("org.springdoc:springdoc-openapi-kotlin:$springDocVersion")
+  implementation("org.springdoc:springdoc-openapi-data-rest:$springDocVersion")
 }
 
 java {
